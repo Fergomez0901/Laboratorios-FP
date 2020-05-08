4fb054821db93a4f7084cffc89db90641070b1eb
Fergomez0901 <00147420@uca.edu.sv>
2020-05-08 13:52:41 -0600

    update

--- Tarea Laboratorio 3/ejercicio3.cpp
+++ Tarea Laboratorio 3/ejercicio3.cpp
@@ -1,10 +1,11 @@
+//Resolver ecuaciones cuadráticas
 #include <iostream>
 #include <math.h>
 using namespace std;
 
 int main(void)
 {
-    cout << "Resolver ecuaciones cuadráticas: " << endl;
+    cout << "Resolver ecuaciones cuadraticas: " << endl;
     float x1, x2, a, b, c;
     cout << "Introduzca los valores de a, b y c, respectivamente: "<< endl;
     
