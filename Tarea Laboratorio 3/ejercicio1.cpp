0220bf70c3bd70952f08b780b2c0509a6cb0cfcd
Fergomez0901 <00147420@uca.edu.sv>
2020-05-09 11:04:31 -0600

    Tarea FINAL

--- Tarea Laboratorio 3/ejercicio1.cpp
+++ Tarea Laboratorio 3/ejercicio1.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //Programa que calule el promedio de tres numeros ingresados.
 #include <iostream>
 #include <math.h>
@@ -16,22 +16,3 @@ using namespace std;
     cout << "El promedio es: "<<promedio;
     return 0;
 }
-=======
-//Programa que calule el promedio de tres numeros ingresados.
-#include <iostream>
-#include <math.h>
-
-using namespace std;
-
- int main(void)
- {
-    float a, b, c, promedio;
-    cout << "Ingrese tres numeros numeros: " << endl;
-
-    cin >>a>>b>>c;
-    
-    promedio = (a+b+c)/3.0;
-    cout << "El promedio es: "<<promedio;
-    return 0;
-}
->>>>>>> ee2522613af29b9ccdefadd013cd13dd631d672a
--- Tarea Laboratorio 3/ejercicio6.cpp
+++ Tarea Laboratorio 3/ejercicio6.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Compra de productos. 
 #include <iostream>
 #include <math.h>
@@ -66,72 +65,3 @@ int main(void)
         return 0;
 }
 
-=======
-//Compra de productos. 
-#include <iostream>
-#include <math.h>
-using namespace std;
-
-int main(void)
-{
-    float manzana = 0.25;
-    float churros = 0.40;
-    float gaseosa = 0.60;
-    float chocolate = 1.00;
-    float hamburguesa = 1.50;
-    int cantidad = 0; 
-
-    cout << "Bienvenidos a mi tienda! \n Productos: \n Manzana = $0.25 \n Churros = $0.40 \n Gaseosa = $0.60 \n Chocolate = $1.00 \n Hamburguesa = $1.50 \n";
-
-    string producto = "";
-    cout << "Que producto desea?" << endl;
-    cin >> producto; 
-
-    if (producto == "manzana" || producto == "Manzana"|| producto == "MANZANA") 
-    {
-        cout << "Que cantidad desea?" << endl;
-        cin >> cantidad; 
-
-        float total = manzana*cantidad;
-        cout << "El total a cancelar es: $" << total;
-    }
-    else if (producto == "churros" || producto == "Churros"|| producto == "CHURROS") 
-        {
-            cout << "Que cantidad desea?" << endl;
-            cin >> cantidad; 
-
-        float total = churros*cantidad;
-        cout << "El total a cancelar es: $" << total;
-        }
-
-        else if (producto == "gaseosa" || producto == "Gaseosa"|| producto == "GASEOSA") 
-        {
-            cout << "Que cantidad desea?" << endl;
-            cin >> cantidad; 
-
-        float total = gaseosa*cantidad;
-        cout << "El total a cancelar es: $" << total;
-        }
-
-        else if (producto == "chocolate" || producto == "Chocolate" || producto =="CHOCOLATE")
-        {
-            cout << "Que cantidad desea?" << endl;
-            cin >> cantidad; 
-
-        float total = chocolate*cantidad;
-        cout << "El total a cancelar es: $" << total;
-        }
-
-        else if (producto == "hamburguesa" || producto == "Hamburguesa" || producto == "HAMBURGUESA")
-        {
-        cout << "Que cantidad desea?" << endl;
-        cin >> cantidad; 
-
-        float total = hamburguesa*cantidad;
-        cout << "El total a cancelar es: $" << total;
-        }
-
-        return 0;
-}
-
->>>>>>> ee2522613af29b9ccdefadd013cd13dd631d672a
