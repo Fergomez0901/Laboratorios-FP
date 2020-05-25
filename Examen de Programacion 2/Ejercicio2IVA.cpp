483ff12a3a4b489627c1eef04c8b842e4f91fb5c
Fergomez0901 <00147420@uca.edu.sv>
2020-05-25 10:14:04 -0600

    Examen Corto 2

--- Examen de Programacion 2/Ejercicio2IVA.cpp
+++ Examen de Programacion 2/Ejercicio2IVA.cpp
@@ -16,33 +16,40 @@ int main(void)
     cin >> opcion; 
     switch (opcion)
     {
+
     case 1:
     cout << "Ingrese el monto al que desea calcular el IVA" << endl;
     cin >> monto; 
     iva = monto * 0.13;
     cout << "El IVA es: $" << iva; 
     break; 
+
     case 2: 
     cout << "Ingrese el monto al que desea calcular el IVA" << endl;
     cin >> monto; 
     iva = monto * 0.09;
     cout << "El IVA es: $" << iva; 
     break; 
+
    case 3: 
     cout << "Ingrese el monto al que desea calcular el IVA" << endl;
     cin >> monto; 
     iva = monto * 0.15;
     cout << "El IVA es: $" << iva; 
     break; 
+
     case 4: 
     cout << "Ingrese el monto al que desea calcular el IVA" << endl;
     cin >> monto; 
     iva = monto * 0.20;
     cout << "El IVA es: $" << iva;  //simbolo de dolar porque el del euro no funciona
     break; 
+
     default:
     cout << "El numero ingresado no es valido" << endl; 
-        break;
+    break;
+
     }
+    
     return 0;
 }
