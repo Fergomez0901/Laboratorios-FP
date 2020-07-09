1e8666d1a12dc2144c35003e94465ea515e2bd8f
Fergomez0901 <00147420@uca.edu.sv>
2020-07-09 16:20:01 -0600

    second look

--- Examen Final FP/facturacion.cpp
+++ Examen Final FP/facturacion.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <cmath>
+#include <string.h>
 using namespace std;
 
 const int longCad = 20; 
 
 struct costoxArticulo
 {
-    char nombreArticulo[longCad + 1];
+    string nombreArticulo;
     int cantidad; 
-    float precio;
+    float precioTotal;
     float costoPorArticulo;
-}factura1;
+};
 
-void llenararticulos();
+void llenararticulos(string, int, float, float);
 
 int main(void)
 {
-    cout << "Cuantos articulos desea comprar?" << endl; 
-    cin >> factura1.cantidad;
+    costoxArticulo f1;  //f1 variable de struct costoxArticulo
+
+llenararticulos(f1.nombreArticulo,f1.cantidad, f1.precioTotal, f1.costoPorArticulo);
 
 
+return 0; 
 }
 
 
-
-void llenararticulos()
+void llenararticulos(string nombreArticulo, int cantidad, float precioTotal, float costo)
 {
-    cout << endl << "Digite el nombre de los artículos a comprar: ."
+    cout << endl << "Ingrese el nombre del Articulo: " << endl;
+    cin >> nombreArticulo;
+    cout << endl << "Ingrese la cantidad de productos a comprar:" << endl; 
+    cin >> cantidad;
+    cout << endl << "Ingrese el costo por unidad:" << endl;
+    cin >> costo;
+
+    precioTotal = cantidad*costo;
+    cout << endl << "El precio total es: $" << precioTotal;
+
 }
